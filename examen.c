f73207c218e8767a2363eb8ec94f27a842f76a7e
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 16:14:28 -0500

    Implementación de las funciones dentro del código para su impresión

--- examen.c
+++ examen.c
@@ -2,39 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-int alumnos[23];
 float CpromedioAlumno(int notas[]);
 float CpromedioGrupal(int notas[][3], int);
 int CmayorPromedio(float promedio[]);
-
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
+    int alumnos[23];
     int notas[23][3], mejorAlumno;
     float promedioAlumno[23], promedioGrupal[3];
 
     for (int i = 0; i < 23; i++)
     {
-        printf("Alumno %d:\n", i + 1);
-
+        printf("Alumno %d: \n", i + 1);
         for (int j = 0; j < 3; j++)
         {
-            alumnos[23] = rand() % 16;
-            printf("Progreso %d \n: ", j + 1);
-            scanf("%d\t", &notas[i][j]);
+            printf("Progreso %d: \n", j + 1);
+            notas[23][3]=rand() % 6;
         }
-        printf("/n");
+        printf("\n");
         promedioAlumno[i] = CpromedioAlumno(notas[i]);
+    }
+    
+    for (int i = 0; i < 1; i++)
+    {
+        promedioAlumno[i] = CpromedioAlumno(notas);
+        printf("Promedio individual por Semestre: %2.f\n", promedioAlumno[i]);
     }
 
     for (int j = 0; j < 3; j++)
     {
         promedioGrupal[j] = CpromedioGrupal(notas, j);
-        printf("Promedio grupal por Progreso %d: %2.f\n", j + 1, promedioGrupal[j]);
+        printf("Promedio grupal Progreso %d: %2.f\n", j + 1, promedioGrupal[j]);
     }
 
     mejorAlumno = CmayorPromedio(promedioAlumno);
-    printf("El mejor alumno es el número %d con un promedio de %.2f\n", mejorAlumno + 1, promedioAlumno[mejorAlumno]);
+    printf("El mejor alumno es el numero %d con un promedio de: %.2f\n", mejorAlumno + 1, promedioAlumno[mejorAlumno]);
 
     return 0;
 }
@@ -55,7 +58,7 @@ float CpromedioGrupal(int notas[][3], int progreso)
 {
     float suma = 0;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 23; i++)
     {
         suma += notas[i][progreso];
     }
