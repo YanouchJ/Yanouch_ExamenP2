cf12c95f0b706fd94eb5030eeca979ec57243d3c
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-12-06 15:33:44 -0500

    Impllementación de las funciones para calcular el promedio semestral, promedio grupal y el mayor promedio

--- examen.c
+++ examen.c
@@ -3,26 +3,77 @@
 #include <time.h>
 
 int alumnos[23];
-float promedioAlumno(int notas[]);
-float promedioGrupal(int notas[][3]);
-float mayorPromedio (int promedio[]);
-
+float CpromedioAlumno(int notas[]);
+float CpromedioGrupal(int notas[][3], int);
+int CmayorPromedio(float promedio[]);
 
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    
-    
-    for (int i = 0; i < 23; i++)
-    {
-        alumnos[23]= rand() % 21;
-    }
-    
+    int notas[23][3], mejorAlumno;
+    float promedioAlumno[23], promedioGrupal[3];
 
     for (int i = 0; i < 23; i++)
     {
-        printf("%d\n", alumnos[23]);
+        printf("Alumno %d:\n", i + 1);
+
+        for (int j = 0; j < 3; j++)
+        {
+            alumnos[23] = rand() % 16;
+            printf("Progreso %d \n: ", j + 1);
+            scanf("%d\t", &notas[i][j]);
+        }
+        printf("/n");
+        promedioAlumno[i] = CpromedioAlumno(notas[i]);
     }
+
+    for (int j = 0; j < 3; j++)
+    {
+        promedioGrupal[j] = CpromedioGrupal(notas, j);
+        printf("Promedio grupal por Progreso %d: %2.f\n", j + 1, promedioGrupal[j]);
+    }
+
+    mejorAlumno = CmayorPromedio(promedioAlumno);
+    printf("El mejor alumno es el número %d con un promedio de %.2f\n", mejorAlumno + 1, promedioAlumno[mejorAlumno]);
 
     return 0;
+}
+
+float CpromedioAlumno(int notas[])
+{
+    float suma = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        suma += notas[i];
+    }
+
+    return suma / 3;
+}
+
+float CpromedioGrupal(int notas[][3], int progreso)
+{
+    float suma = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        suma += notas[i][progreso];
+    }
+
+    return suma / 23;
+}
+
+
+int CmayorPromedio(float promedio[])
+{
+int mejorAlumno=0;
+
+for (int i = 0; i < 23; i++)
+{
+    if (promedio[i] > promedio[mejorAlumno])
+    {
+        mejorAlumno= i;
+    }
+}
+return mejorAlumno;
 }
